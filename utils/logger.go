b8e5034b2551c5cb354119e5a08899e9e2a2dd4f
Jamie Harvey <jamie@harvey.earth>
2024-09-08 11:26:29 -0700

    move logging functions out of errors file since they belong more in logger and add debug log to connection

--- utils/logger.go
+++ utils/logger.go
@@ -1,6 +1,10 @@
 package utils
 
 import (
+	"fmt"
+	"strings"
+	"unicode"
+
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -52,3 +56,47 @@ func InitLogger() {
 func LogRequest(r []byte) {
 	Debug(string(r))
 }
+
+func cleanup(errString string, err error) string {
+	errString = strings.Map(func(r rune) rune {
+		if unicode.IsGraphic(r) {
+			return r
+		}
+		return -1
+	}, errString)
+	return fmt.Sprintf(strings.Replace(errString, "\n ", "", 1), err.Error())
+}
+
+// Debug returns a debug message to the logger
+func Debug(debugString string) {
+	Elilogger.Debug(debugString)
+}
+
+// Error returns an error message to the logger and terminates
+func Error(errString string, err error) error {
+	errString = cleanup(errString, err)
+	if err != nil {
+		Elilogger.Error(errString)
+	}
+	return nil
+}
+
+// Fatal returns a fatal msg to the logger and terminates
+func Fatal(errString string, err error) {
+	errString = cleanup(errString, err)
+	if err != nil {
+		Elilogger.Fatal(errString)
+	}
+}
+
+// Info returns an info msg to the logger
+// is info or debug
+func Info(infoString string) {
+	Elilogger.Info(infoString)
+}
+
+// Warn returns a warning msg to the logger
+func Warn(warnString string) {
+	Elilogger.Warn(warnString)
+}
+
