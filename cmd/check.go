76c2055160b36f4c101cabe0f361ed52c2e198ff
Jamie Harvey <jamie@harvey.earth>
2024-09-10 12:17:15 -0700

    refactored check command

--- cmd/check.go
+++ cmd/check.go
@@ -1,16 +1,9 @@
 package cmd
 
 import (
-	"context"
-	"errors"
-	"io"
-	"net/http"
-	"os"
-	"path/filepath"
-
-	"github.com/elastic/go-elasticsearch/v8/esapi"
 	"github.com/spf13/cobra"
 
+	"github.com/harvey-earth/elilogs/internal"
 	"github.com/harvey-earth/elilogs/utils"
 )
 
@@ -26,45 +19,10 @@ EXIT STATUS
 	Run: func(cmd *cobra.Command, args []string) {
 		utils.Info("check called")
 
-		// connect to ES instance
-		conn, err := utils.Connect()
+		c, _ := cmd.Flags().GetBool("cache")
+		err := internal.Check(c)
 		if err != nil {
-			utils.Error("error connecting", err)
-		}
-		utils.Info("check successful")
-
-		if c, _ := cmd.Flags().GetBool("cache"); c {
-			utils.Info("getting cache")
-
-			// Make request for indexes and error check
-			indexResp, err := esapi.CatIndicesRequest{Format: "json"}.Do(context.Background(), conn)
-			if err != nil {
-				utils.Error("error requesting indexes:", err)
-			}
-			if indexResp.StatusCode != http.StatusOK {
-				r, _ := io.ReadAll(indexResp.Body)
-				utils.Error("error requesting indexes:", errors.New(string(r)))
-			}
-
-			resp, _ := io.ReadAll(indexResp.Body)
-			utils.LogRequest(resp)
-			indexData, err := utils.HandleResponse(resp)
-			if err != nil {
-				utils.Error("error unmarshalling response:", err)
-			}
-
-			// Get user home directory and write to .cache/elilogs.txt
-			// TODO: Add error handling if .cache doesn't exist
-			homePath, _ := os.UserHomeDir()
-			filePath, _ := filepath.Abs(homePath + "/.cache/elilogs.txt")
-			f, err := os.Create(filePath)
-			defer f.Close()
-
-			for i := 0; i < len(indexData); i++ {
-				f.WriteString(indexData[i]["index"])
-				f.WriteString("\n")
-			}
-			utils.Info("created cache")
+			utils.Error("Error checking connection", err)
 		}
 	},
 }
